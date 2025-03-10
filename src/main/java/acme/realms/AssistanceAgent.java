e714e4340c7b6be1c65750f951a4ffec41124cae
Nicolas Parrilla <nicolasparrillageniz@gmail.com>
2025-03-10 11:16:45 +0100

    Task 26: Assistance Agent Entity
    He creado la entidad assistanceAgent.
    
    Refs #34

---
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -0,0 +1,63 @@
+
+package acme.realms;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.datatypes.Moment;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AssistanceAgent extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				employeeCode;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				spokenLanguagues;
+
+	@Mandatory
+	@ValidString
+	@Automapped
+	private String				airline;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Moment				moment;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				bio;
+
+	@Optional
+	@ValidMoney
+	@Automapped
+	private Money				salary;
+
+	@Optional
+	@ValidString
+	@Automapped
+	private String				pictureUrl;
+
+}
