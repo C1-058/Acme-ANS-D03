310e9d70bc63f1852ef68e567ce18b734de70852
Javier Luque <javluqrui@alum.us.es>
2025-03-12 17:47:55 +0100

    Task 049: Airport Test
    Refs #89

--- src/main/java/acme/entities/purchase/Purchase.java
+++ src/main/java/acme/entities/purchase/Purchase.java
@@ -5,7 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
+import acme.client.components.basis.AbstractEntity;
 import acme.client.components.validation.Mandatory;
+import acme.entities.flight.Flight;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,7 +16,7 @@ import lombok.Setter;
 @Getter
 
 @Setter
-public class Purchase {
+public class Purchase extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
 
