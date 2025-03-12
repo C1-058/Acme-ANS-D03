5adc88f0f3d137dfc5d8ffe6f55c946be9a01d55
MaxCorti <cortimax03@gmail.com>
2025-03-12 16:40:35 +0100

    Fix extension
    Refs #88

--- src/main/java/acme/entities/purchase/Purchase.java
+++ src/main/java/acme/entities/purchase/Purchase.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
+import acme.client.components.basis.AbstractEntity;
 import acme.client.components.validation.Mandatory;
 import acme.entities.flight.Flight;
 import acme.realms.Customer;
@@ -16,7 +17,7 @@ import lombok.Setter;
 @Getter
 
 @Setter
-public class Purchase {
+public class Purchase extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
 
