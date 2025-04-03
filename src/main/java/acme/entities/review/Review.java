0bbebcb65a0a46c3a5ee98816801befd55d79177
Pablo Vargas Sáez <pvargas04@hotmail.com>
2025-04-03 23:41:41 +0200

    Features del student 5 para la entrega 3

--- src/main/java/acme/entities/review/Review.java
+++ src/main/java/acme/entities/review/Review.java
@@ -48,10 +48,10 @@ public class Review extends AbstractEntity {
 	@Optional
 	@ValidNumber(min = 0, max = 10, fraction = 2)
 	@Automapped
-	private double				score;
+	private Double				score;
 
 	@Optional
 	// @Valid by default
 	@Automapped
-	private boolean				recommended;
+	private Boolean				recommended;
 }
